6a334aeecfb5212be85e4b48a8c7b8018f9516d8
Sumanth0410 <127470232+Sumanth0410@users.noreply.github.com>
2023-11-24 11:55:21 +0530

    Add files via upload

---
+++ 3.script.js
@@ -0,0 +1,13 @@
+class Person{
+    constructor(firstName, lastName, age, gender, maritalStatus, phoneNo, emailId){
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+      this.gender = gender;
+      this.maritalStatus = maritalStatus;
+      this.phoneNo = phoneNo;
+      this.emailId = emailId;
+    }
+  }
+  
+  let ram = new Person("sumanth", "g", 22, male , "Single",7395902954, "sumanthgopu0410@gmail.com")
